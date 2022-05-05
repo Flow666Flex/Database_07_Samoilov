986613d46b2810385ec11d272a3264f76170c7c6
Dima Samoilov <dimasikman3@mail.ru>
2022-05-05 12:49:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/database_07/DB.java
@@ -0,0 +1,60 @@
+package com.example.database_07;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DB extends SQLiteOpenHelper {
+    public DB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE my_test (my_key TEXT, my_value TEXT);"; //creating table with 2 columns
+        db.execSQL(sql);
+    }
+
+    public void do_insert(String key, String value) {
+
+        String sql = "INSERT INTO my_test VALUES ('" + key + "', '" + value + "');"; //insert new row into table
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(sql);
+    }
+
+    public String do_select(String key) {
+
+        String sql = "SELECT my_value FROM my_test WHERE my_key = '" + key + "';"; //finds value inside table by given key
+        SQLiteDatabase db = getWritableDatabase();//get ready to read from database
+        Cursor cur = db.rawQuery(sql, null); //run query and acquire results in a new "table" with only 1 column
+
+        if (cur.moveToFirst() == true) //jump to the first (and the only one) matching record, if possible
+            return cur.getString(0); // return value from the first column (my_value)
+
+        return "Не найдено";
+    }
+
+    public String do_update(String key, String value) {
+
+        String sql = "UPDATE my_test SET my_value = ' " + value + "' WHERE my_key = '" + key + "';"; //update matching records
+        SQLiteDatabase db = getWritableDatabase();//get ready to read from database
+        db.execSQL(sql);
+        return "Запись обновлена";
+    }
+
+    public String do_delete(String key) {
+
+        String sql = "DELETE FROM my_test WHERE my_key = '" + key + "';"; //delete matching records
+        SQLiteDatabase db = getWritableDatabase();//get ready to read from database
+        db.execSQL(sql);
+        return "Этой записи больше нет";
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
---
+++ app/src/main/java/com/example/database_07/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.database_07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt_key;
+    TextView txt_value;
+    DB mydb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt_key = findViewById(R.id.txt_key);
+        txt_value = findViewById(R.id.txt_value);
+        //create new or open database file "mybase.db" with version 1
+        mydb = new DB(this, "mybase.db",null,1);
+    }
+
+    public void onButtonInsert_Click(View v) {
+        String key = txt_key.getText().toString();
+        String value = txt_value.getText().toString();
+        mydb.do_insert(key,value);
+    }
+
+    public void onButtonUpdate_Click(View v) {
+        String key = txt_key.getText().toString();
+        String value = txt_value.getText().toString();
+        txt_value.setText(mydb.do_update(key,value));
+    }
+
+    public void onButtonSelect_Click(View v) {
+        String key = txt_key.getText().toString();
+        String value = mydb.do_select(key);
+        txt_value.setText(value);
+    }
+
+    public void onButtonDelete_Click(View v) {
+        String key = txt_key.getText().toString();
+        txt_value.setText(mydb.do_delete(key));
+    }
+}
